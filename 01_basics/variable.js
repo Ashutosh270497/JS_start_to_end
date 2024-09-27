df5c31648bf845b4ce161a7203fcc099ae27c0e7
Ashutosh Tiwari <40489739+Ashutosh270497@users.noreply.github.com>
2024-09-27 14:31:50 +0000

    learning variables

---
+++ 01_basics/variable.js
@@ -0,0 +1,7 @@
+const accountId = 144232
+let accountEmail = "ashu@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
